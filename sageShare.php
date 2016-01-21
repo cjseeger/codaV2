562a10fcb6d6c0b6342b8b9726d62fe3e9b485b6
cjseeger <cjseeger@iastate.edu>
2016-01-21 16:04:47 -0600

    sample

---
+++ sageShare.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<script>
+
+</script>
+
+
+<style>
+	.myText {
+	color: yellow;
+	font-family: fantasy;
+	font-size: 30px;
+
+}
+	</style>
+
+</head>
+
+<body>
+
+
+<b>This is Sage</b>.<br>
+This is Chris Seeger and I am typing here.<br><br>
+<span class = 'myText' >Hello Class. This is fun<br></span>
+
+
+<h3>I'm Bob.</h3>
+<h4> I am Fredrick</h4>
+
+	hi<br>
+
+	<h4> hi</h4>
+
+
+</body>
+</html>
+
+
+
+
+
